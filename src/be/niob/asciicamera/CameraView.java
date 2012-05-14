49d826b4872825a2819de2d9f09a49d1de1ea653
Sander Versluys <versluyssander@gmail.com>
2012-05-14 09:03:40 +0200

    try rendering ascii from rgba byte source

--- src/be/niob/asciicamera/CameraView.java
+++ src/be/niob/asciicamera/CameraView.java
@@ -2,6 +2,10 @@ package be.niob.asciicamera;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 
 /**
  * 
@@ -17,15 +21,25 @@ class CameraView extends CameraViewBase {
 	int[] mRGBA;
 	private Bitmap mBitmap;
     private int mViewMode;
+    private Paint mPaint;
     
     public static final int     VIEW_MODE_RGBA = 0;
     public static final int     VIEW_MODE_GRAY = 1;
     public static final int		VIEW_MODE_ASCII = 2;
     
+    private static final int SIDE = 20;
+    
+    private static final char[] ascii = " .`-_':,;^=+/\"|)\\<>)iv%xclrs{*}I?!][1taeo7zjLunT#JCwfy325Fp6mqSghVd4EgXPGZbYkOA&8U$@KHDBWNMR0Q".toCharArray();
+    
     public CameraView(Context context) {
         super(context);
         mSize = 0;
         mViewMode = VIEW_MODE_RGBA;
+        
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+		mPaint.setTypeface(Typeface.MONOSPACE);
+		mPaint.setTextSize(SIDE);
     }
 
     @Override
@@ -40,8 +54,9 @@ class CameraView extends CameraViewBase {
                 int y = (0xff & ((int) data[i]));
                 rgba[i] = 0xff000000 + (y << 16) + (y << 8) + y;
             }
+            mBitmap.setPixels(rgba, 0/* offset */, getFrameWidth() /* stride */, 0, 0, getFrameWidth(), getFrameHeight());
         } else if (view_mode == VIEW_MODE_RGBA) {
-            for (int i = 0; i < getFrameHeight(); i++)
+            for (int i = 0; i < getFrameHeight(); i++) {
                 for (int j = 0; j < getFrameWidth(); j++) {
                 	int index = i * getFrameWidth() + j;
                 	int supply_index = frameSize + (i >> 1) * getFrameWidth() + (j & ~1);
@@ -61,11 +76,47 @@ class CameraView extends CameraViewBase {
 
                     rgba[i * getFrameWidth() + j] = 0xff000000 + (b << 16) + (g << 8) + r;
                 }
+            }
+            mBitmap.setPixels(rgba, 0/* offset */, getFrameWidth() /* stride */, 0, 0, getFrameWidth(), getFrameHeight());
         } else if (view_mode == VIEW_MODE_ASCII) {
+        	
+        	Canvas canvas = new Canvas(mBitmap);
+        	canvas.drawColor(Color.WHITE);
+        	
+        	for (int i = 0; i < frameSize; i++) {
+                int y = (0xff & ((int) data[i]));
+                rgba[i] = 0xff000000 + (y << 16) + (y << 8) + y;
+            }
+            mBitmap.setPixels(rgba, 0/* offset */, getFrameWidth() /* stride */, 0, 0, getFrameWidth(), getFrameHeight());
+        	
+        	/*int wx = mBitmap.getWidth() / SIDE;
+			int wy = mBitmap.getHeight() / SIDE;*/
+            
+            int wx = getFrameWidth() / SIDE;
+			int wy = getFrameHeight() / SIDE;
+        	
+        	for (int x=0; x<wx; x++) {
+				for (int y=0; y<wy; y++) {
+					
+					
+					
+					int p = mBitmap.getPixel(x*SIDE, y*SIDE+SIDE);
+					int r = Color.red(p);
+					int g = Color.green(p);
+					int b = Color.blue(p);
+					double l = (int) (0.2126 * r + 0.7152 * g + 0.0722 * b);
+					int pos = (int)((l / 256) * ascii.length);
+					canvas.drawText(Character.toString(ascii[pos]), x*SIDE, y*SIDE+SIDE, mPaint);
+					
+					//canvas.drawText("S", x*SIDE, y*SIDE+SIDE, mPaint);
+				}
+			}
+        	
+        	
         	
         }
         
-        mBitmap.setPixels(rgba, 0/* offset */, getFrameWidth() /* stride */, 0, 0, getFrameWidth(), getFrameHeight());
+        
         return mBitmap;
     }
 
